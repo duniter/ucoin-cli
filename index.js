81056cb510d578044eb401d5cefacfec0ea3d40e
cgeek <cem.moreau@gmail.com>
2013-08-07 23:05:41 +0200

    Add pks requests

---
+++ index.js
@@ -0,0 +1,100 @@
+var _       = require('underscore');
+var fs      = require('fs');
+var openpgp = require('./openpgp').openpgp;
+
+var pub = fs.readFileSync('/home/cgeek/dev/vucoin/looolcat.pub', 'utf8');
+// console.log(pub);
+openpgp.init();
+openpgp.keyring.importPublicKey(pub);
+
+module.exports = function(server){
+
+  this.pks = {
+
+    add: function (key, signature, done) {
+      post('/pks/add', done)
+      .form({
+        "keytext": fs.createReadStream(key),
+        "keysign": fs.createReadStream(signature)
+      });
+    },
+
+    lookup: function (search, done) {
+      get('/pks/lookup?search=' + encodeURIComponent(search) + '&op=index', done);
+    }
+  }
+
+  function get(url, callback) {
+    return require('request').get({
+      "url": "http://" + server + url,
+      "headers": {
+        "Accept": "multipart/signed"
+      }
+    }, _(vucoin_result).partial(callback));
+  }
+
+  function post(url, callback) {
+    return require('request').post({
+      "url": "http://" + server + url,
+      "headers": {
+        "Accept": "multipart/signed"
+      }
+    }, _(vucoin_result).partial(callback));
+  }
+
+  return this;
+}
+
+function vucoin_result(done, err, res, body) {
+  var result = null;
+  if(err)
+    done(err);
+  else{
+    if(res.statusCode == 200){
+      verifyResponse(res, body, done);
+    }
+    else{
+      errorCode(res, body, done);
+    }
+  }
+}
+
+function verifyResponse(res, body, done) {
+  var type = res.headers["content-type"];
+  if(type && type.match(/multipart\/signed/)){
+    var boundaries = type.match(/boundary=([\w\d]*);/);
+    if(boundaries.length > 1){
+      var boundary = "--" + boundaries[1];
+      body = body.replace(/\r\n/g, '\n').replace(/\n/g, '\r\n');
+      var index1 = body.indexOf(boundary);
+      var index2 = body.indexOf(boundary, boundary.length + index1);
+      var index3 = body.indexOf(boundary, boundary.length + index2);
+      var content = body.substring(boundary.length + '\r\n'.length + index1, index2 - '\r\n'.length*2);
+      var signature = body.substring(boundary.length + '\r\n'.length + index2, index3 - '\r\n'.length*2);
+      signature = "-----BEGIN PGP SIGNED MESSAGE-----\r\nHash: SHA256\r\n\r\n" + content + '\r\n' + signature.substring(signature.lastIndexOf('-----BEGIN PGP SIGNATURE'));
+      var sig = openpgp.read_message(signature)[0];
+      if(sig.verifySignature()){
+        // Correct public keys and signature messages
+        content = content.replace('BEGIN PGP PUBLIC KEY BLOCK', '-----BEGIN PGP PUBLIC KEY BLOCK-----');
+        content = content.replace('BEGIN PGP SIGNATURE', '-----BEGIN PGP SIGNATURE-----');
+        content = content.replace('END PGP PUBLIC KEY BLOCK', '-----END PGP PUBLIC KEY BLOCK-----');
+        content = content.replace('END PGP SIGNATURE', '-----END PGP SIGNATURE-----');
+        done(null, JSON.parse(content));
+      }
+      else done("Signature verification failed");
+    }
+  }
+  else done("Non signed content.");
+}
+
+function errorCode(res, body, done) {
+    if(res.statusCode == 400)
+      err = "400 - Bad request.";
+    else if(res.statusCode == 404)
+      err = "404 - Not found.";
+    else{
+      err = res.statusCode + " - Unknown error.";
+    }
+    err += "\n" + body;
+    done(err);
+}
--- readme.md
+++ readme.md
@@ -2,6 +2,48 @@
 
 Node.js module to access ucoin server through its HTTP API.
 
+## Usage
+
+```js
+var node = require('vucoin')('localhost:8081');
+```
+
+```js
+node.pks.add('/path/to/key.pub', '/path/to/signature', function(err, key){
+  // Key is now updated
+  var fpr = key.fingerprint;
+  var name = key.name;
+})
+```
+
+```js
+node.pks.lookup('John Carter', function(err, keys){
+  // Results
+})
+```
+
+## Command Line
+
+vucoin may be installed globally via
+
+```bash
+$ sudo npm install vucoin -g
+$ vucoin --help
+
+  Usage: vucoin [options]
+
+  Options:
+
+    -h, --help                 output usage information
+    -V, --version              output the version number
+    --add <keyFile>            File of the key to submit.
+    --lookup <search>          Search string
+    -s, --signature <sigFile>  File of a signature to append.
+    -n, --node <address>       DNS, IPv4 or IPv6 address of the node to contact.
+    -p, --port <port>          Port of the node to contact.
+```
+
+
 # License
 
 This software is provided under [MIT license](https://raw.github.com/c-geek/vucoin/master/LICENSE).
