35900fe58959ed26c5d2c9cabdf7ecc4d2276aa8
cgeek <cem.moreau@gmail.com>
2016-03-16 11:50:03 +0100

    Added appveyor support
    Conflicts:
    	package.json

--- postuninstall.sh
+++
@@ -1,6 +0,0 @@
-echo "Remove bash completion script..."
-COMPFILE=/etc/bash_completion.d/ucoin
-if [ "$(whoami)" = "root" ] && [ -e $COMPFILE ] && [ -f $COMPFILE ]; then
-  echo "Remove $COMPFILE"
-  rm $COMPFILE
-fi
